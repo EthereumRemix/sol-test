536678f98ce9f1ad29e7cee6b2ba06a80c6baa08
ioedeveloper <daviddisu8@gmail.com>
2023-03-29 17:10:41 +0100

    Install rlp peer dependencies

--- src/index.ts
+++ src/index.ts
@@ -24,12 +24,12 @@ async function execute () {
     const isNPMrepo = fs.existsSync(packageLock)
 
     if (isYarnRepo) {
-      await cli.exec('yarn', ['global', 'add', '@remix-project/remix-tests'])
+      await cli.exec('yarn', ['global', 'add', 'rlp', '@remix-project/remix-tests@0.2.25-alpha.17'])
     } else if (isNPMrepo) {
-      await cli.exec('npm', ['install', '@remix-project/remix-tests', '-g'])
+      await cli.exec('npm', ['install', 'rlp', '@remix-project/remix-tests@0.2.25-alpha.17', '-g'])
     } else {
       await cli.exec('npm', ['init', '-y'])
-      await cli.exec('npm', ['install', '@remix-project/remix-tests', '-g'])
+      await cli.exec('npm', ['install', 'rlp', '@remix-project/remix-tests@0.2.25-alpha.17', '-g'])
     }
   })
 
