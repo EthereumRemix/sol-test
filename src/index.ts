2a89e13786c83671e54ea3e471392516d9d893ab
ioedeveloper <daviddisu8@gmail.com>
2023-04-24 14:35:50 +0100

    Install missing deps

--- src/index.ts
+++ src/index.ts
@@ -24,12 +24,12 @@ async function execute () {
     const isNPMrepo = fs.existsSync(packageLock)
 
     if (isYarnRepo) {
-      await cli.exec('yarn', ['global', 'add', 'rlp', '@remix-project/remix-tests'])
+      await cli.exec('yarn', ['global', 'add', 'rlp', 'crypto', '@remix-project/remix-tests'])
     } else if (isNPMrepo) {
-      await cli.exec('npm', ['install', 'rlp', '@remix-project/remix-tests', '-g'])
+      await cli.exec('npm', ['install', 'rlp', 'crypto', '@remix-project/remix-tests', '-g'])
     } else {
       await cli.exec('npm', ['init', '-y'])
-      await cli.exec('npm', ['install', 'rlp', '@remix-project/remix-tests', '-g'])
+      await cli.exec('npm', ['install', 'rlp', 'crypto', '@remix-project/remix-tests', '-g'])
     }
   })
 
